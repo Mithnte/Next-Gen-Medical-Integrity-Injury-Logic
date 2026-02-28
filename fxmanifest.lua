c916d96ac2eaa4d626db9250d44998e3902980e4
Mithnte <baehareq@gmail.com>
2026-03-01 06:54:02 +0900

    Add fxmanifest.lua for Advanced Medical System
    Initial creation of fxmanifest.lua for the Advanced Medical System.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Vibe Dev Team'
+description 'Advanced Medical System 2026 - The Ultimate Game Changer'
+
+shared_script '@ox_lib/init.lua' -- WAJIB ADA OX_LIB
+
+client_scripts { 'client.lua' }
+server_scripts { 'server.lua' }
+
+ui_page 'html/index.html'
+files { 'html/index.html' }
